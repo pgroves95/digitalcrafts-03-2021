070074470198a277906682af75fffb7fc120c8de
Patrick Groves <patrickalexgroves@gmail.com>
2021-03-10 13:41:19 -0700

    updates input_loop.py

--- week1/day_3/input_loop.py
+++ week1/day_3/input_loop.py
@@ -1,9 +1,38 @@
-user = input("Please enter your name > ")
-length = len(user)
+#user first name
+first1 = input("Please enter your first name > ")
+len1 = len(first1)
 
-while length < 1:
+while len1 < 1:
     print("Please enter at least one character")
-    user = input("Please enter your name > ")
-    length = len(user)
+    first1 = input("Please enter your first name > ")
+    len1 = len(first1)
 
-print(f"Your name is {user}")
+#user last name
+last1 = input("Please enter your last name > ")
+len2 = len(last1)
+
+while len2 < 1:
+    print("Please enter at least one character")
+    last1 = input("Please enter last your name > ")
+    len2 = len(last1)
+
+#friend first name
+first2 = input("Please enter your friend's first name > ")
+len3 = len(first2)
+
+while len3 < 1:
+    print("Please enter at least one character")
+    first2 = input("Please enter your friend's first name > ")
+    len3 = len(first2)
+
+#friend last name
+last2 = input("Please enter your friend's last name > ")
+len4 = len(last2)
+
+while len4 < 1:
+    print("Please enter at least one character")
+    last2 = input("Please enter your friend's last name > ")
+    len4 = len(last2)
+
+#print all names
+print(f"{first1} {last1} is friend's with {first2} {last2}.")
