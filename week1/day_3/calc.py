7d595dc2a00091058b0d6b68b88f9d03279fad71
Patrick Groves <patrickalexgroves@gmail.com>
2021-03-11 15:25:35 -0700

    created list form of calc

--- week1/day_3/calc.py
+++ week1/day_3/calc.py
@@ -1,29 +1,58 @@
-print("\nThis program performs integer arithmetic.\nIt takes a number, an operator, and another number,\nthen displays the result of the operation.\n")
+print("""This program performs integer arithmetic.
+It takes a number, an operator, and another number,
+then displays the result of the operation.\n""")
 
-num1 = int(input("Type a number > "))
-operator = input("Type an operator (+, -, *, /) > ")
-num2 = int(input("Type another number > "))
+def entry():
 
-def add(x,y):
-    return x + y
-def subtract(x,y):
-    return x - y
-def multiply(x,y):
-    return x * y
-def divide(x,y):
-    return x / y
+    op_list = []
+    prompt = ''
+        
+    prompt = input("Type a number >> ")
+    try:
+        prompt = int(prompt)
+        op_list.append(prompt)
+    except ValueError:
+        print("That is not a valid number, Try again.")
+        return entry()
 
-result = 0
-if operator == '+':
-    result = add(num1, num2) 
-elif operator == '-':
-    result = subtract(num1, num2)
-elif operator == '*':
-    result = multiply(num1, num2)
-elif operator == '/':
-    result = divide(num1, num2)
-else:
-    print("Operator not recognized. Please try again.")
-    exit()
+    prompt = input("\nType an operator >> ")
+    if prompt in ['+', '-', '*', '/']:
+        op_list.append(prompt)
+    else:
+        print("That is not a valid operator. Try again.")
+        return entry()
 
-print(f"The result is: {result}")
+    prompt = input("\nType another number >> ")
+    try:
+        prompt = int(prompt)
+        op_list.append(prompt)
+    except ValueError:
+        print("That is not a valid number, Try again.")
+        return entry()
+
+    return op_list
+
+
+def calc(expression):
+
+    num1 = expression[0]
+    operator = expression[1]
+    num2 = expression[2]
+
+    if operator == '+':
+            return num1 + num2
+    elif operator == '-':
+            return num1 - num2
+    elif operator == '*':
+            return num2 * num2
+    else:
+            return num1 / num2
+
+done = ''
+while done != 'Y':
+
+    expression = entry()
+    print(f"{expression[0]} {expression[1]} {expression[2]}")
+    calc = calc(expression)
+    print(calc)
+    done = input("Type 'Y' to quit, otherwise continue. >> ")
