d3cef19b3119abdd01abb9b80d2613a2bc88a2f0
Patrick Groves <patrickalexgroves@gmail.com>
2021-03-12 10:04:58 -0700

    removes whitespace in fizzbuzz

--- week1/day_3/fizz_buzz.py
+++ week1/day_3/fizz_buzz.py
@@ -4,13 +4,10 @@
 for num in range (0,101):
     if num % 3 == 0 and num % 5 == 0:
         print("fizz buzz")
-
     elif num % 3 == 0:
         print("fizz")
-
     elif num % 5 == 0:
         print("buzz")
-
     else:
         print(num)
 
