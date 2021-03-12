68203317aa61b74f3f0179f8682337e376ef8397
Patrick Groves <patrickalexgroves@gmail.com>
2021-03-12 08:26:27 -0700

    fixes fizzbuzz and adds sum of multiples alg

--- week1/day_3/fizz_buzz.py
+++ week1/day_3/fizz_buzz.py
@@ -1,21 +1,16 @@
-print("""
-This program takes an integer. If the number
-is divisible by 3, it will return 'fizz'. If
-the number is divisible by 5, it will return 'buzz'.
-If the number is divisible by both 3 and 5,
-it will return 'fizz buzz'.\n""")
 
-num = int(input("Please enter an integer > "))
 
-if num % 3 == 0 and num % 5 == 0:
-    print("fizz buzz")
 
-elif num % 3 == 0:
-    print("fizz")
+for num in range (0,101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizz buzz")
 
-elif num % 5 == 0:
-    print("buzz")
+    elif num % 3 == 0:
+        print("fizz")
 
-else:
-    print("The given integer is\nnot divisible by 3 nor 5.")
+    elif num % 5 == 0:
+        print("buzz")
+
+    else:
+        print(num)
 
