cf5ce66c57659df4afc226680e007d511425ea08
Patrick Groves <patrickalexgroves@gmail.com>
2021-03-29 13:37:40 -0600

    adds js files including calc function

---
+++ week4/day_1/calc.js
@@ -0,0 +1,13 @@
+function calc(num1, num2, operator) {
+    if(operator === "+") {
+        return num1 + num2
+    } else if(operator === "-") {
+        return num1 - num2
+    } else if(operator === "*") {
+        return num1 * num2
+    } else if(operator === "/") {
+        return num1 / num2
+    } else {
+        return "invalid operator"
+    }
+}   
