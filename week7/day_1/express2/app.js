154c577a356a81dd589c4b8c1f8c9d79246bda6e
Patrick Groves <patrickalexgroves@gmail.com>
2021-04-19 13:36:49 -0600

    linking html pages in express without temp/view

--- week7/day_1/express2/app.js
+++ week7/day_1/express2/app.js
@@ -1,7 +1,42 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
+const { readFile } = require("fs")
 
+
+app.get('/', (req,res) => {
+    res.send("Welcome to my page.")
+})
+
+app.get('/home', (req,res) => {
+    readFile("./html/home.html", "utf-8", (err, data) => {
+        res.send(data)
+    })
+})
+
+app.get('/faq', (req,res) => {
+    readFile("./html/faq.html", "utf-8", (err, data) => {
+        res.send(data)
+    })
+})
+
+app.get('/about', (req,res) => {
+    readFile("./html/about.html", "utf-8", (err, data) => {
+        res.send(data)
+    })
+})
+
+app.get('/messages', (req, res) => {
+    readFile("./html/messages.html", "utf-8", (err, data) => {
+        res.send(data)
+    })
+})
+
+app.get("*", (req, res) => {
+    readFile("./html/unfound.html", "utf-8", (err, data) => {
+        res.send(data)
+    })
+})
 
 
 app.listen(PORT, (req,res) => {
