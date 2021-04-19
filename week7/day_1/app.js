42d35e8ecfc8431ed8756ea9838041f11a96f736
Patrick Groves <patrickalexgroves@gmail.com>
2021-04-19 10:14:42 -0600

    html via express

--- week7/day_1/app.js
+++ week7/day_1/app.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const address = "127.0.0.1"
 const PORT = 3000
+// const fs = require('fs')
+const {readFile} = require('fs')
+// destructuring allows access to values inside of modules like functions, classes etc.
 
 app.get('/', (req,res) => {
-    res.end("Hello")
+    readFile('./index.html', 'utf-8', (err,data) => {
+        if(err){
+            res.send("doesn't exist")
+        }
+        res.send(data)
+    })
 })
 
 app.get('/b', (req,res) => {
