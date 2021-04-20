e6d6735d819fb101f9edfe0b0bce15bf93b558c3
Patrick Groves <patrickalexgroves@gmail.com>
2021-04-20 09:37:51 -0600

    post test

--- week7/day_2/app.js
+++ week7/day_2/app.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
 const PORT = 3000;
+app.use(express.json())
 
 app.get('/', (req,res) =>{
     res.send("I'm here at root.")
+})
+
+app.get('/about', (req,res) =>{
+    res.send("I'm here at about.")
+})
+
+app.get('/faq', (req,res) =>{
+    res.send("I'm here at FAQ.")
+})
+
+app.get('/team', (req, res) => {
+    res.send('team page')
+})
+
+app.post('/team', (req,res) =>{
+    console.log(req.body.name)
 })
 
 app.listen(PORT, (req,res) => {
