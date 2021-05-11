6ccac481926be3c54ac2e17b3e0856b0919fb51d
Patrick Groves <patrickalexgroves@gmail.com>
2021-05-11 13:57:31 -0600

    .

--- week9/day_2/app-demos/state-mgmt/src/House.js
+++ week9/day_2/app-demos/state-mgmt/src/House.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import Garage from './Garage'
 
 export default class House extends Component {
+        
+    
     render() {
         return (
-            <div>
-                
-            </div>
+            <>
+              <Garage />  
+            </>
         )
     }
 }
