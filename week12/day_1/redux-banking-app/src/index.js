4532d3e323de5ca2fe659038b892d7a04b0e67e4
Patrick Groves <patrickalexgroves@gmail.com>
2021-05-24 08:10:00 -0600

    redux store with multiple state vars

--- week12/day_1/redux-banking-app/src/index.js
+++ week12/day_1/redux-banking-app/src/index.js
@@ -1,14 +1,58 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
+import { createStore } from 'redux';
 
 console.log("starting banking app")
+
+const defaultState = {
+    checking: 100,
+    savings: 100
+};
+
+const ACTION_DEPOSIT = 'deposit'
+const ACTION_WITHDRAWAL = 'withdrawal'
+
+function createDeposit(acct, amt) {
+    return {
+        type: ACTION_DEPOSIT,
+        payload: {
+            acct,
+            amt
+        }
+    };
+}
+
+function createWithdrawal(acct,amt){
+    return {
+        type: ACTION_WITHDRAWAL,
+        payload: {
+            acct,
+            amt
+        }
+    };
+}
+
+function accounts(state=defaultState, action) {
+    // eslint-disable-next-line default-case
+    switch(action.type) {
+        case ACTION_DEPOSIT:
+            return {
+                ...state,
+                [action.payload.acct]: state[action.payload.acct] + action.payload.amt
+            }
+        case ACTION_WITHDRAWAL:
+            return {
+                [action.payload.acct]: state[action.payload.acct] - action.payload.amt
+            }
+    }
+    return state;
+}
+
+const store = createStore(accounts);
+store.subscribe(() => {
+    console.log('=== state has updated ===')
+    const state = store.getState();
+    console.log(state)
+})
+
+window.store = store
+window.createWithdrawal = createWithdrawal
+window.createDeposit = createDeposit
