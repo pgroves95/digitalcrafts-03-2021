55aecc471f42ffd2058acf3c08eab2bfe756b820
Patrick Groves <patrickalexgroves@gmail.com>
2021-05-25 10:04:25 -0600

    router mvp

--- week12/day_2/rct-router/src/App.js
+++ week12/day_2/rct-router/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import AboutMe from './components/AboutMe'
+import Blog from './components/Blog'
+import Home from './components/Home'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/aboutme'>About Me</Link>
+          <Link to='/blog'>Blog</Link>
+        </nav>
+      </div>
+      <Switch>
+        <Route path='/aboutme'>
+          <AboutMe />
+        </Route>
+        <Route path='/blog'>
+          <Blog />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
---
+++ week12/day_2/rct-router/src/components/AboutMe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutMe() {
+    return (
+        <div>
+            <h1>AboutMe</h1>
+        </div>
+    )
+}
