b815d6456fd16262178912e6cb9d3f8b728fd5a5
Patrick Groves <patrickalexgroves@gmail.com>
2021-05-26 08:58:57 -0600

    redux pkgs

--- week12/day_3/redux-demo/src/components/Counter.js
+++ week12/day_3/redux-demo/src/components/Counter.js
@@ -4,8 +4,8 @@ export default function Counter(props) {
     return (
         <div>
             <h1>Count: {props.count}</h1>
-            <button onClick={()=>props.inc}>plus one</button>
-            <button onClick={()=>props.dec}>minus one</button>
+            <button onClick={()=>props.inc()}>plus one</button>
+            <button onClick={()=>props.dec()}>minus one</button>
         </div>
     )
 }
