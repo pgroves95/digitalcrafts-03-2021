219256b438496098533dfc5484436b557ac5590b
Patrick Groves <patrickalexgroves@gmail.com>
2021-06-07 10:28:04 -0600

    .

--- week13/day_5/context-demo/src/App.js
+++ week13/day_5/context-demo/src/App.js
@@ -1,9 +1,33 @@
+import React, { useState } from 'react';
 import './App.css';
+import AddUser from './components/AddUser';
+import UserList from './components/UserList';
+import { AppContext } from './context'
 
 function App() {
+
+  const [ users, setUsers ] = useState([])
+
+  const dispatchUserEvent = (actionType, payload) => {
+    switch(actionType) {
+      case 'ADD_USER':
+        setUsers([...users, payload.newUser])
+        return
+      case 'REMOVE_USER':
+        setUsers(users.filter(user=>user.id !== payload.userid))
+        return
+      default:
+        return
+    }
+  }
+
+
   return (
     <div className="App">
-
+      <AppContext.Provider value={{ users }}>
+        <AddUser dispatch={dispatchUserEvent}/>
+        <UserList dispatch={dispatchUserEvent}/>
+      </AppContext.Provider>
     </div>
   );
 }
---
+++ week13/day_5/context-demo/src/components/AddUser.js
@@ -0,0 +1,28 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context';
+
+const AddUser = (props) => {
+    const [name, setName] = useState("")
+    const [age, setAge] = useState("")
+    const [bio, setBio] = useState("")
+
+    const handleAddUser = () => {
+        const user = {id: Math.random(),name,age,bio}
+        dispatch('ADD_USER',{newUser: user})
+    }
+
+    return (
+        <div>
+            <h3>Add New User</h3>
+            <input type="text" value={name} onChange={e=>{setName(e.target.value)}} placeholder="name"/>
+            <br/>
+            <input type="text" value={age} onChange={e=>{setAge(e.target.value)}} placeholder="age"/>
+            <br/>
+            <textarea type="text" value={bio} onChange={e=>{setBio(e.target.value)}} placeholder="bio"/>
+            <br/>
+            <button onClick={handleAddUser}>Add User</button>
+        </div>
+    )
+}
+
+export default AddUser
